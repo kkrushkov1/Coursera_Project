0b6ef2fa5e00609f9adc0786292fa9aab56e6b6c
kkrushkov1 <k.krushkov1@gmail.com>
2023-02-14 11:06:24 +0200

    Updated files & added an optional feature

--- src/main/java/com/example/coursera_project/Coursera_Project.java
+++ src/main/java/com/example/coursera_project/Coursera_Project.java
@@ -24,6 +24,12 @@ public class Coursera_Project {
         String startDate = inputArray[1];
         String endDate = inputArray[2];
         String reportsPath = inputArray[3];
+        String outputFormat;
+        if (inputArray.length >= 5 && !inputArray[4].isEmpty()) {
+            outputFormat = inputArray[4];
+        } else {
+            outputFormat = "both";
+        }
 
 
         try {
@@ -45,67 +51,78 @@ public class Coursera_Project {
                     "WHERE c.credit >= "
                     + requiredCredit + " AND scx.completion_data BETWEEN '" + startDate + "' AND '" + endDate + "'");
 
-            // Generate the HTML report
-            StringBuilder htmlReport = new StringBuilder("<html>" +
-                    "<body>" +
-                    "<table>" +
-                    "<tr>" +
-                    "<th>Student Name</th>" +
-                    "<th>Course Name</th>" +
-                    "<th>Time</th>" +
-                    "<th>Credit</th>" +
-                    "<th>Instructor</th>" +
-                    "</tr>");
-            while (resultSet.next()) {
-                String studentName = resultSet.getString("Name");
-                String courseName = resultSet.getString("CourseName");
-                int time = resultSet.getInt("Time");
-                int credits = resultSet.getInt("Credit");
-                String instructorName = resultSet.getString("InstructorName");
-
-                htmlReport.append("<tr><td>")
-                        .append(studentName).append("</td><td>")
-                        .append(courseName).append("</td><td>")
-                        .append(time).append("</td><td>")
-                        .append(credits).append("</td><td>")
-                        .append(instructorName).append("</td></tr>");
+            if (outputFormat.equalsIgnoreCase("csv") || outputFormat.equalsIgnoreCase("both")) {
+                generateCsvReport(resultSet, reportsPath);
             }
-            htmlReport.append("</table></body></html>");
-
-            // Save the HTML report to file
-            File htmlFile = new File(reportsPath + "/report.html");
-            FileWriter htmlWriter = new FileWriter(htmlFile);
-            htmlWriter.write(htmlReport.toString());
-            htmlWriter.close();
-
-            // Generate the CSV report
-            StringBuilder csvReport = new StringBuilder("Student Name, Course Name, Time, Credits, Instructor\n");
-            resultSet.beforeFirst();
-            while (resultSet.next()) {
-                String studentName = resultSet.getString("Name");
-                String courseName = resultSet.getString("CourseName");
-                int time = resultSet.getInt("Time");
-                int credits = resultSet.getInt("Credit");
-                String instructorName = resultSet.getString("InstructorName");
-
-                csvReport.append(studentName).append(",")
-                        .append(courseName).append(",")
-                        .append(time).append(",")
-                        .append(credits).append(",")
-                        .append(instructorName).append("\n");
+            if (outputFormat.equalsIgnoreCase("html") || outputFormat.equalsIgnoreCase("both")) {
+                generateHtmlReport(resultSet, reportsPath);
             }
-
-            // Save the CSV report to file
-            File csvFile = new File(reportsPath + "/report.csv");
-            FileWriter csvWriter = new FileWriter(csvFile);
-            csvWriter.write(csvReport.toString());
-            csvWriter.close();
-
             System.out.println("Reports generated successfully");
         } catch (SQLException | IOException e) {
             System.out.println("Error generating reports: " + e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
+    }
+
+    private static void generateHtmlReport(ResultSet resultSet, String reportsPath) throws Exception {
+        // Generate the HTML report
+        StringBuilder htmlReport = new StringBuilder("<html>" +
+                "<body>" +
+                "<table>" +
+                "<tr>" +
+                "<th>Student Name</th>" +
+                "<th>Course Name</th>" +
+                "<th>Time</th>" +
+                "<th>Credit</th>" +
+                "<th>Instructor</th>" +
+                "</tr>");
+        while (resultSet.next()) {
+            String studentName = resultSet.getString("Name");
+            String courseName = resultSet.getString("CourseName");
+            int time = resultSet.getInt("Time");
+            int credits = resultSet.getInt("Credit");
+            String instructorName = resultSet.getString("InstructorName");
+
+            htmlReport.append("<tr><td>")
+                    .append(studentName).append("</td><td>")
+                    .append(courseName).append("</td><td>")
+                    .append(time).append("</td><td>")
+                    .append(credits).append("</td><td>")
+                    .append(instructorName).append("</td></tr>");
+        }
+        htmlReport.append("</table></body></html>");
+
+        // Save the HTML report to file
+        File htmlFile = new File(reportsPath + "/report.html");
+        FileWriter htmlWriter = new FileWriter(htmlFile);
+        htmlWriter.write(htmlReport.toString());
+        htmlWriter.close();
+    }
+
+    private static void generateCsvReport(ResultSet resultSet, String reportsPath) throws Exception {
+        StringBuilder csvReport = new StringBuilder("Student Name, Course Name, Time, Credits, Instructor\n");
+        resultSet.beforeFirst();
+        while (resultSet.next()) {
+            String studentName = resultSet.getString("Name");
+            String courseName = resultSet.getString("CourseName");
+            int time = resultSet.getInt("Time");
+            int credits = resultSet.getInt("Credit");
+            String instructorName = resultSet.getString("InstructorName");
+
+            csvReport.append(studentName).append(",")
+                    .append(courseName).append(",")
+                    .append(time).append(",")
+                    .append(credits).append(",")
+                    .append(instructorName).append("\n");
+        }
+
+        // Save the CSV report to file
+        File csvFile = new File(reportsPath + "/report.csv");
+        FileWriter csvWriter = new FileWriter(csvFile);
+        csvWriter.write(csvReport.toString());
+        csvWriter.close();
     }
 
 }
